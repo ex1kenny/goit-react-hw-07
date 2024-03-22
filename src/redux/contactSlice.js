774b68f3f822c284f3dc75033aed0458fddfc492
ex1kenny <ex1kenny4543@gmail.com>
2024-03-22 20:55:43 +0200

    home-work7

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  fetchContact,
+  addContact,
+  deleteContact,
+  updateContact,
+} from "./contactOps";
+
+const contactsSlice = createSlice({
+  name: "contacts",
+  initialState: {
+    items: [],
+    loading: false,
+    error: false,
+  },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchContact.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchContact.rejected, () => {})
+      .addCase(addContact.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items.push(action.payload);
+      })
+      .addCase(addContact.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      })
+      .addCase(deleteContact.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = state.items.filter(
+          (item) => item.id !== action.payload.id
+        );
+      })
+      .addCase(deleteContact.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      })
+      .addCase(updateContact.fulfilled, (state, action) => {
+        const contactToUpdate = state.items.findIndex(
+          (item) => item.id === action.payload.id
+        );
+        state.items[contactToUpdate] = action.payload;
+      }),
+});
+
+export const contactReducer = contactsSlice.reducer;
